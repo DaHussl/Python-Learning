c52b88c67c77b8f91f51487c4659946892f606c0
DaHussl <hussam.alkobati@yahoo.de>
2023-05-07 21:52:21 +0200

    learn variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+# Bei einem "=" übernimmt Links den Wert von Rechts
+age = 24
+
+# Text Ausgabe in Anführungsstrichen
+# Wenn es eine Konstante werden soll dann wird die Variable GROß geschrieben
+NAME = "Hussam"
+
+# Das "f" steht für Format = Alles was in Anführungsstrichen und Geschweiftenklammern steht wird wie eine Variable behandelt
+#  {} = trennen die Varaible vom Text
+print (f"{NAME} is {age} years old")
+
+# Die Funktion "type" gibt den Typen einer Variable aus
+print (type(NAME))
+
+print (type(age))
