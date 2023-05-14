65676618b9dee46b265636edc8a3b599dcc91168
DaHussl <hussam.alkobati@yahoo.de>
2023-05-14 22:05:44 +0200

    extra Exercise

---
+++ Basics/extraAufgabe.py
@@ -0,0 +1,9 @@
+Zahl1 = int(input("Gebe eine Zahl ein: "))
+Zahl2 = int(input("Gebe noch eine Zahl ein: "))
+
+print(Zahl1+Zahl2)
+print(Zahl1-Zahl2)
+print(Zahl1*Zahl2)
+print(Zahl1/Zahl2)
+print(Zahl1%Zahl2)
+print(Zahl1**Zahl2)
